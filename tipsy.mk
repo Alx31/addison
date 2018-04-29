6d9d1b8591df661f947e61dc6288a2f04526498a
Alx31 <alex31.couderc@gmail.com>
2018-04-29 10:22:29 +0200

    tipsy plz

--- tipsy.mk
+++ tipsy.mk
@@ -22,7 +22,8 @@ TARGET_SCREEN_HEIGHT := 1920
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := addison
-PRODUCT_NAME := validus_addison
+PRODUCT_MODEL := Moto Z Play
+PRODUCT_NAME := tipsy_addison
 PRODUCT_BRAND := motorola
 PRODUCT_MANUFACTURER := motorola
 
